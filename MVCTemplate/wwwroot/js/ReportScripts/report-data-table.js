a9ebf0a94e4694b7e8392bf8c6942bdc6c19974b
Chezsi <depazchezsie02@gmail.com>
2025-06-09 11:49:57 +0800

    moving of script for filter (report)

--- MVCTemplate/wwwroot/js/ReportScripts/report-data-table.js
+++ MVCTemplate/wwwroot/js/ReportScripts/report-data-table.js
@@ -15,62 +15,6 @@
         }
     });
 
-    // External filters to search DataTable columns
-    $('#titleSearch').on('keyup change', function () {
-        $('#reportTable').DataTable().column(0).search(this.value).draw();
-    });
-
-    $('#descriptionSearch').on('keyup change', function () {
-        $('#reportTable').DataTable().column(2).search(this.value).draw();
-    });
-
-    // Export filtered PDF button event handler
-    $('#exportFilteredPdfBtn').on('click', function () {
-        var table = $('#reportTable').DataTable();
-        var titleFilter = table.column(0).search() || '';
-        var descriptionFilter = table.column(2).search() || '';
-
-        var url = '/Admin/Report/ExportFilteredToPdf';
-        var queryParams = [];
-
-        if (titleFilter) {
-            queryParams.push('titleFilter=' + encodeURIComponent(titleFilter));
-        }
-        if (descriptionFilter) {
-            queryParams.push('descriptionFilter=' + encodeURIComponent(descriptionFilter));
-        }
-
-        if (queryParams.length > 0) {
-            url += '?' + queryParams.join('&');
-        }
-
-        window.open(url, '_blank');
-    });
-
-    // Export filtered Excel button event handler
-    $('#exportFilteredExcelBtn').on('click', function () {
-        var table = $('#reportTable').DataTable();
-        var titleFilter = table.column(0).search() || '';
-        var descriptionFilter = table.column(2).search() || '';
-
-        var url = '/Admin/Report/ExportFilteredToExcel';
-        var queryParams = [];
-
-        if (titleFilter) {
-            queryParams.push('titleFilter=' + encodeURIComponent(titleFilter));
-        }
-        if (descriptionFilter) {
-            queryParams.push('descriptionFilter=' + encodeURIComponent(descriptionFilter));
-        }
-
-        if (queryParams.length > 0) {
-            url += '?' + queryParams.join('&');
-        }
-
-        window.open(url, '_blank');
-    });
-});
-
 function loadDataTableReport() {
     dataTable = $('#reportTable').DataTable({
         "ajax": {
---
+++ MVCTemplate/wwwroot/js/ReportScripts/report-search.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+    // Initially hide export filtered buttons
+    $('#exportFilteredPdfBtn, #exportFilteredExcelBtn').hide();
+
+    function toggleExportButtons() {
+        const titleVal = $('#titleSearch').val().trim();
+        const descVal = $('#descriptionSearch').val().trim();
+
+        if (titleVal.length > 0 || descVal.length > 0) {
+            $('#exportFilteredPdfBtn, #exportFilteredExcelBtn').show();
+        } else {
+            $('#exportFilteredPdfBtn, #exportFilteredExcelBtn').hide();
+        }
+    }
+
+    // External inputs filter from js code columns
+    $('#titleSearch').on('keyup change', function () {
+        $('#reportTable').DataTable().column(0).search(this.value).draw();
+        toggleExportButtons();
+    });
+
+    $('#descriptionSearch').on('keyup change', function () {
+        $('#reportTable').DataTable().column(2).search(this.value).draw();
+        toggleExportButtons();
+    });
+
+    // Export filtered PDF button event handler
+    $('#exportFilteredPdfBtn').on('click', function () {
+        var table = $('#reportTable').DataTable();
+        var titleFilter = table.column(0).search() || '';
+        var descriptionFilter = table.column(2).search() || '';
+
+        var url = '/Admin/Report/ExportFilteredToPdf';
+        var queryParams = [];
+
+        if (titleFilter) {
+            queryParams.push('titleFilter=' + encodeURIComponent(titleFilter));
+        }
+        if (descriptionFilter) {
+            queryParams.push('descriptionFilter=' + encodeURIComponent(descriptionFilter));
+        }
+
+        if (queryParams.length > 0) {
+            url += '?' + queryParams.join('&');
+        }
+
+        window.open(url, '_blank');
+    });
+
+    // Export filtered Excel button event handler
+    $('#exportFilteredExcelBtn').on('click', function () {
+        var table = $('#reportTable').DataTable();
+        var titleFilter = table.column(0).search() || '';
+        var descriptionFilter = table.column(2).search() || '';
+
+        var url = '/Admin/Report/ExportFilteredToExcel';
+        var queryParams = [];
+
+        if (titleFilter) {
+            queryParams.push('titleFilter=' + encodeURIComponent(titleFilter));
+        }
+        if (descriptionFilter) {
+            queryParams.push('descriptionFilter=' + encodeURIComponent(descriptionFilter));
+        }
+
+        if (queryParams.length > 0) {
+            url += '?' + queryParams.join('&');
+        }
+
+        window.open(url, '_blank');
+    });
+});
