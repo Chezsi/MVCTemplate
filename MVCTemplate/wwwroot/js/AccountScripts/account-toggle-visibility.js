c00ba84b9666c8bc15367c0e1a6f4c27e144d91a
Chezsi <depazchezsie02@gmail.com>
2025-07-28 15:16:16 +0800

    Modify - Admin/Account - Scripts Moved

---
+++ MVCTemplate/wwwroot/js/AccountScripts/account-toggle-visibility.js
@@ -0,0 +1,9 @@
+﻿$(document).on('click', '#togglePassword', function () {
+    const passwordInput = $('#editPassword');
+    const icon = $(this).find('i');
+
+    const type = passwordInput.attr('type') === 'password' ? 'text' : 'password';
+    passwordInput.attr('type', type);
+
+    icon.toggleClass('fa-eye fa-eye-slash');
+});
